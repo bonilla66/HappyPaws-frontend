371f2644e788b1e8198520041dee27d39f77cfb9
Pz0324 <00062019@uca.edu.sv>
2025-06-19 17:01:17 -0600

    conecting historial aplications and aplication accepted by user

--- happy-paws-frontend/src/services/AdoptService.js
+++ happy-paws-frontend/src/services/AdoptService.js
@@ -7,12 +7,10 @@ export const submitApplication = async (data, token) => {
     },
   });
 };
-// Actualizar estado de solicitud
 export const updateAplicationState = (id, newState) => {
   return api.put(`/aplication/${id}`, { aplicationStatus: newState });
 };
 
-// Borrar solicitud
 export const deleteAplication = (id) => {
   return api.delete(`/aplication/${id}`);
 };
