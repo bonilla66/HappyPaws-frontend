54c4870567c8c3305c07141bfe7dc3af0ef4dfde
Pz0324 <00062019@uca.edu.sv>
2025-06-27 20:01:50 -0600

    prueba

--- happy-paws-frontend/src/services/api.js
+++ happy-paws-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://happypaws-backend-latest.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
 
